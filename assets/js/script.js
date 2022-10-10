2179eae9dc662730c479b4f047a2cee3a249e4dc
UlissesJunior <ulissesjunior2005@gmail.com>
2022-10-10 11:28:19 -0300

    add and pull data from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 const menu = document.querySelector("#AddProfile")
 const profiles = document.querySelector(".profiles")
+const CreatedProfiles = JSON.parse(localStorage.getItem("Profiles")) || []
+
+
+CreatedProfiles.forEach ((elemento) => {
+    CreateProfile(elemento)
+})
+
+
 function EditMenu () {   
     menu.classList.toggle("menu-open")
 }
@@ -7,26 +15,39 @@ function EditMenu () {
 function SendProfile () {
     var nome = document.querySelector("#nome")
     var img = document.querySelector("#img")
-    CreateProfile(nome.value,img.value)
+
+    const NewProfile = {
+        "nome": nome.value,
+        "img": img.value
+    }
+    
+    CreateProfile(NewProfile)
+
+    CreatedProfiles.push(NewProfile)
+    LocalStorageAdd(CreatedProfiles)
+
     menu.classList.toggle("menu-open")
     alert("Dados salvos com sucesso!")
 }
 
-function CreateProfile (nome, img) {
-    console.log(nome, img)
+function CreateProfile (Profile) {
     const novoItem = document.createElement("div")
     novoItem.classList.add("profiles-column")
 
     const nomeItem = document.createElement('div')
     nomeItem.classList.add("card-notes")
-    nomeItem.innerHTML = nome
+    nomeItem.innerHTML = Profile.nome
 
     const div = document.createElement('img')
-    div.src = img
+    div.src = Profile.img
     const parents = div.classList.add("card-profiles")
     
     novoItem.insertBefore(div, parents)
     novoItem.appendChild(nomeItem)
 
     profiles.appendChild(novoItem)
+}
+
+function LocalStorageAdd (CreatedProfiles) {
+    localStorage.setItem("Profiles", JSON.stringify(CreatedProfiles))
 }
